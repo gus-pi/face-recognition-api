4c1a321a0315975828e134921f8ba817c2b57d55
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-11-26 16:15:01 -0400

    add signin and register endpoints

--- server.js
+++ server.js
@@ -2,8 +2,56 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 app.get('/', (req, res) => {
   res.send('this is working');
+});
+
+app.post('/signin', (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json('success');
+  } else {
+    res.status(400).json('wrong email or password');
+  }
+});
+
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
 });
 
 app.listen(3001, () => {
