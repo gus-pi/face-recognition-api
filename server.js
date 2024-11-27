4f83c58f63383e3fe028773d2ea8efeb94ea38a6
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-11-27 14:23:17 -0400

    install cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 //app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 const database = {
   users: [
