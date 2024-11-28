b31a9ecdb9032ac2e35d61281485229b6b083d51
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-11-28 14:41:51 -0400

    add login when registering

--- server.js
+++ server.js
@@ -65,17 +65,29 @@ app.post('/register', async (req, res) => {
   //hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-  db('users')
-    .returning('*')
-    .insert({
-      name: name,
-      email: email,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) => res.status(400).json('Unable to register'));
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hashedPassword,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then((loginEmail) => {
+        trx('users')
+          .returning('*')
+          .insert({
+            name: name,
+            email: loginEmail[0].email,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json('Unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
