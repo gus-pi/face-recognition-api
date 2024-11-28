b6073fa98a64e69a8afb835ce9634c106f5c0377
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-11-28 14:23:12 -0400

    add database to image endpoint

--- server.js
+++ server.js
@@ -81,7 +81,8 @@ app.post('/register', async (req, res) => {
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
   let found = false;
-  db.select('*')
+  db('users')
+    .select('*')
     .from('users')
     .where({
       id: id,
@@ -101,17 +102,16 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(404).json('User not found.');
-  }
+  db('users')
+    .where({
+      id: id,
+    })
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json('Error getting entries'));
 });
 
 const CLARIFAI_API_URL =
